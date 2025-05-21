6a8964864f4e45e0b01f36c5823d7e2e7c9b6234
Sahib_zade <quluzadsahib55@gmail.com>
2025-05-21 19:37:58 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,53 @@
 
-import './App.css'
-import { Routes, Route, NavLink } from 'react-router-dom'
-import Fulltime from './Fulltime';
-import Stopwatch from './Stopwatch';
-import Timer from './Timer';
+import React, { useState } from "react";
+import Pageheader from "./Pageheader";
+import MovieList from "./MovieList";
+import SearchInput from "./SearchInput";
+import FavoriteList from "./FavoriteList";
+import "./App.css";
+
 function App() {
-  
+  const [activeView, setActiveView] = useState("movies");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [favorites, setFavorites] = useState([]);
+  const [favoriteListName, setFavoriteListName] = useState("");
 
   return (
-    <>
-
-   <div className='pages'>
-    <NavLink to='/'>Fulltime</NavLink>
-    <NavLink to='/about'>Stopwatch</NavLink>
-    <NavLink to='/workexamples'>Timer</NavLink>
-   </div>
-
-
-      <Routes>
-        <Route path='/' element={<Fulltime />} />
-        <Route path='/about' element={<Stopwatch />} />
-        <Route path='/workexamples' element={<Timer />} />
-      </Routes>
-    </>
-  )
+    <div className="App">
+      <Pageheader />
+      {activeView === "movies" ? (
+        <div className="movies-container">
+          <div className="movies-section">
+            <SearchInput setSearchTerm={setSearchTerm} />
+            <MovieList
+              searchTerm={searchTerm}
+              favorites={favorites}
+              setFavorites={setFavorites}
+            />
+          </div>
+          <div className="favorites-section">
+            <FavoriteList
+              favorites={favorites}
+              setFavorites={setFavorites}
+              activeView={activeView}
+              setActiveView={setActiveView}
+              favoriteListName={favoriteListName}
+              setFavoriteListName={setFavoriteListName}
+            />
+          </div>
+        </div>
+      ) : (
+        <FavoriteList
+          favorites={favorites}
+          setFavorites={setFavorites}
+          activeView={activeView}
+          setActiveView={setActiveView}
+          favoriteListName={favoriteListName}
+          setFavoriteListName={setFavoriteListName}
+        />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
